430579368e22bf216207c420e7881b9a5d227a19
IdoLeybo <Idol@moveo.co.il>
2021-11-25 22:35:14 +0200

    Create Dish & restaurant Modals

--- client/src/components/admin/Dishes.tsx
+++ client/src/components/admin/Dishes.tsx
@@ -1,18 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Dish from "./Dish";
 import { dish } from "../../interfaces/index.interface";
+import CreateForm from "./Modal/CreateModals/CreateForm";
 
 export default function Dishes(props: any) {
+  const [isModal, setIsModal] = useState(false);
+
   const dishes = props.dishes;
   const restaurants = props.restaurants;
+
+  const openCreateForm = () => {
+    setIsModal(!isModal);
+  };
   return (
     <div>
       <h1>Dishes</h1>
+      <button onClick={openCreateForm}>Add New Dish</button>
       <div style={{ display: "flex", flexDirection: "row" }}>
         {dishes.map((dish: dish, index: any) => {
           return <Dish key={index} details={dish} restaurants={restaurants} />;
         })}
       </div>
+      {isModal && (
+        <CreateForm
+          openCreateForm={openCreateForm}
+          restaurants={restaurants}
+          modalType="dish"
+        />
+      )}
     </div>
   );
 }
--- client/src/components/admin/Restaurants.tsx
+++ client/src/components/admin/Restaurants.tsx
@@ -1,18 +1,33 @@
 import React, { useState } from "react";
 import Restaurant from "./Restaurant";
 import { restaurant } from "../../interfaces/index.interface";
+import CreateForm from "./Modal/CreateModals/CreateForm";
 
 export default function Restaurants(props: any) {
+  const [isModal, setIsModal] = useState(false);
   const restaurants = props.restaurants;
+  const chefs = props.chefs;
+
+  const openCreateForm = () => {
+    setIsModal(!isModal);
+  };
 
   return (
     <div>
       <h1>Restaurants</h1>
+      <button onClick={openCreateForm}>Add New Restaurant</button>
       <div style={{ display: "flex", flexDirection: "row" }}>
         {restaurants.map((restaurant: restaurant) => {
           return <Restaurant key={restaurant.id} details={restaurant} />;
         })}
       </div>
+      {isModal && (
+        <CreateForm
+          openCreateForm={openCreateForm}
+          chefs={chefs}
+          modalType="restaurant"
+        />
+      )}
     </div>
   );
 }
